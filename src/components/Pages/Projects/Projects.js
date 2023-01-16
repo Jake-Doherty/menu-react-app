7d8184909fe38913f7d9355aeb6353d7079837c4
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-15 22:16:19 -0800

    import project data and pass to Project component

--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -1,6 +1,28 @@
 import React from "react";
 import "./Projects.css";
+import { projects } from "../../../projects-data.js";
+import Project from "../../Project/Project.js";
 
 export default function Projects() {
-    return <div>Projects</div>;
+    return (
+        <>
+            <section className="projects-section">
+                <ul className="projects-list">
+                    {projects.map((project) => {
+                        return (
+                            <Project
+                                key={project.projectName}
+                                name={project.projectName}
+                                projectUrl={project.deployedUrl}
+                                repoUrl={project.repoUrl}
+                                projectImage={project.projectImage}
+                                about={project.projectAbout}
+                                contributors={project.projectContributors}
+                            />
+                        );
+                    })}
+                </ul>
+            </section>
+        </>
+    );
 }
