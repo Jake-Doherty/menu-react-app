bb5891b8ad7b24cbd45f433c603ec8c9c6fb60e1
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-25 10:29:52 -0800

    removes old code relating to nav and rendered component

--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -8,25 +8,9 @@ import About from "../../Pages/About/About.js";
 import Projects from "../../Pages/Projects/Projects.js";
 import Contact from "../../Pages/Contact/Contact.js";
 
-export default function Main({ nav }) {
-    // const renderPageComponent = () => {
-    //     if (nav === "home") {
-    //         return <Home />;
-    //     }
-    //     if (nav === "about") {
-    //         return <About />;
-    //     }
-    //     if (nav === "projects") {
-    //         return <Projects />;
-    //     }
-    //     if (nav === "contact") {
-    //         return <Contact />;
-    //     }
-    // };
-
+export default function Main() {
     return (
         <main>
-            {/* {renderPageComponent()} */}
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
