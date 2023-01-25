86974f07adca38f21efe43deac16674b8e1a17d9
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-24 23:29:42 -0800

    removes nav and toggle state for now

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-import React from "react";
 import "./App.css";
 import Footer from "./components/Layout/Footer/Footer.js";
 import Header from "./components/Layout/Header/Header.js";
 import Main from "./components/Layout/Main/Main.js";
 
-import background from "./background.gif";
-import { useState } from "react";
-
-function setStyles() {
-    document.body.style.backgroundImage = `url(${background})`;
-    // document.body.style.backgroundImage = "none";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundPosition = "center";
-}
+// hopefully this doesn't mess things up!!!!
 
 function App() {
-    setStyles();
-    const [toggle, setToggle] = useState(true);
-
-    const [nav, setNav] = useState("home");
-
     return (
         <div className="App">
-            <Header
-                toggle={toggle}
-                setToggle={setToggle}
-                nav={nav}
-                setNav={setNav}
-            />
-            <Main nav={nav} />
+            <Header />
+            <Main />
             <Footer />
         </div>
     );
