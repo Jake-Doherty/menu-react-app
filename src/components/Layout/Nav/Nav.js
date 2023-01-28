420bacde5d88601c1498e70445ee820207ffaa12
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-28 13:59:01 -0800

    changes styles and adds DOM selectors for new nav menu using NavLink

--- src/components/Layout/Nav/Nav.js
+++ src/components/Layout/Nav/Nav.js
@@ -3,9 +3,32 @@ import { NavLink } from "react-router-dom";
 import "./Nav.css";
 
 export default function Nav() {
+    const handleNavToggle = () => {
+        const navToggleBtn = document.getElementById("nav-toggle-btn");
+        const navContainer = document.getElementById("nav-container");
+        const middleBtnLine = document.getElementById("middle-btn-line");
+        navContainer.classList.toggle("nav-collapse");
+        navContainer.classList.toggle("nav-open");
+        navToggleBtn.classList.toggle("toggle-btn-open");
+        navToggleBtn.classList.toggle("toggle-btn-close");
+        middleBtnLine.classList.toggle("btn-line-middle");
+    };
+
     return (
         <>
             <nav id="nav-container" className="nav-collapse">
+                <div
+                    onClick={handleNavToggle}
+                    className="toggle-btn-open"
+                    id="nav-toggle-btn"
+                >
+                    <span className="btn-line"></span>
+                    <span
+                        id="middle-btn-line"
+                        className="btn-line btn-line-middle"
+                    ></span>
+                    <span className="btn-line"></span>
+                </div>
                 <NavLink
                     id="menu-btn"
                     style={({ isActive }) => ({
