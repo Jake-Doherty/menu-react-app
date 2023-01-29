e1d4561119c271170bdf93d44cf50ab9160cfe9e
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-28 20:17:06 -0800

    adds canvas element and calls starscape hook

--- src/components/Layout/Main/Main.js
+++ src/components/Layout/Main/Main.js
@@ -8,16 +8,38 @@ import About from "../../Pages/About/About.js";
 import Projects from "../../Pages/Projects/Projects.js";
 import Contact from "../../Pages/Contact/Contact.js";
 import NotFound from "../../Pages/NotFound/NotFound.js";
+import { useStarScape } from "../../../hooks/useStarScape.js";
 
 export default function Main() {
+    const { starScape } = useStarScape();
+
+    function componentDidMount() {
+        window.addEventListener("load", starScape);
+    }
+
+    componentDidMount();
+
     return (
         <main>
+            <canvas id="canvas"></canvas>
             <Routes>
-                <Route exact path="/" element={<Home />} />
-                <Route exact path="/about" element={<About />} />
-                <Route exact path="/projects" element={<Projects />} />
-                <Route exact path="/contact" element={<Contact />} />
-                <Route path="*" element={<NotFound />} />
+                <Route exact path="/" element={<Home {...{ starScape }} />} />
+                <Route
+                    exact
+                    path="/about"
+                    element={<About {...{ starScape }} />}
+                />
+                <Route
+                    exact
+                    path="/projects"
+                    element={<Projects {...{ starScape }} />}
+                />
+                <Route
+                    exact
+                    path="/contact"
+                    element={<Contact {...{ starScape }} />}
+                />
+                <Route path="*" element={<NotFound {...{ starScape }} />} />
             </Routes>
         </main>
     );
