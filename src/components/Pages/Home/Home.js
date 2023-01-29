bceacc58650c00486d81787cc2e17107fa8b8b33
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-28 16:01:22 -0800

    moves callback into context then calls context

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,29 +1,21 @@
-import React, { useCallback } from "react";
+import React, { useContext } from "react";
 import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import { tsParticles } from "tsparticles-engine";
-import { loadPolygonMaskPlugin } from "tsparticles-plugin-polygon-mask";
-import options from "../../../particles-options-data.js";
-
+import { ParticlesContext } from "../../../context/ParticlesContext.js";
+import { useHomeParticles } from "../../../hooks/useHomeParticles.js";
 import "./Home.css";
 
 export default function Home() {
-    const particlesInit = useCallback(async (engine) => {
-        await loadFull(engine);
-        await loadPolygonMaskPlugin(tsParticles);
-        await tsParticles.load("tsparticles", options);
-    }, []);
+    const { particlesInit, particlesLoaded, particles, setParticles } =
+        useContext(ParticlesContext);
 
-    const particlesLoaded = useCallback(async (container) => {
-        await console.log(container);
-    }, []);
+    useHomeParticles(setParticles);
 
     return (
         <Particles
             id="tsparticles"
             init={particlesInit}
             loaded={particlesLoaded}
-            options={options}
+            options={particles}
         />
     );
 }
