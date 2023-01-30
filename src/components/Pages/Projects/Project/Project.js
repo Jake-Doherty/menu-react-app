e10fe527feb348ad5a0793da70d1d00aee9e05d6
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-29 21:09:56 -0800

    changes the way projects are displayed

--- src/components/Pages/Projects/Project/Project.js
+++ src/components/Pages/Projects/Project/Project.js
@@ -14,45 +14,43 @@ export default function Project({
     return (
         <>
             <li className="project">
-                <div className="project-name-image-container">
-                    <img
-                        className="project-image"
-                        // src/assets/project-images/dummies-logo.png
-                        src={`/project-images/${projectImage}.png`}
-                        alt={projectName}
-                    />
-                    <div>
-                        <h3 className="project-name">{projectName}</h3>
-                        <div className="anchor-flexbox">
-                            <a
-                                className="project-anchors"
-                                href={deployedUrl}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                Site
-                            </a>
-                            <span> | </span>
-                            <a
-                                className="project-anchors"
-                                href={repoUrl}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                Repo
-                            </a>
-                        </div>
-                        <div className="contributors-container">
-                            <p className="contributor-p">
-                                {contributors.map((contributor, i) => {
-                                    if (contributors.length - 1 === i) {
-                                        return contributor;
-                                    } else {
-                                        return `${contributor}, `;
-                                    }
-                                })}
-                            </p>
-                        </div>
+                <img
+                    className={`project-image ${projectImage}`}
+                    // src/assets/project-images/dummies-logo.png
+                    src={`/project-images/${projectImage}.png`}
+                    alt={projectName}
+                />
+                <div>
+                    <h3 className="project-name">{projectName}</h3>
+                    <div className="anchor-flexbox">
+                        <a
+                            className="project-anchors"
+                            href={deployedUrl}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Site
+                        </a>
+                        <span> | </span>
+                        <a
+                            className="project-anchors"
+                            href={repoUrl}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Repo
+                        </a>
+                    </div>
+                    <div className="contributors-container">
+                        <p className="contributor-p">
+                            {contributors.map((contributor, i) => {
+                                if (contributors.length - 1 === i) {
+                                    return contributor;
+                                } else {
+                                    return `${contributor}, `;
+                                }
+                            })}
+                        </p>
                     </div>
                 </div>
                 <p className="about-project">{projectAbout}</p>
