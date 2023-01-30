7ed10b120f2065bb598c9facf8cab81f4481cdab
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-29 18:59:51 -0800

    fixes more styles on pages adds linkedin and github icons with links to contact page and fixes more styles on the about page where elements clash with one another on resize

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,30 +1,40 @@
 import React from "react";
 import "./Contact.css";
 
+import linkedin from "../../../assets/social-icons/linkedin.png";
+import github from "../../../assets/social-icons/github-sign.png";
+
 export default function Contact() {
     return (
         <>
             <section className="contact-section">
                 <div className="social-container">
-                    <img
-                        className="social-icon"
-                        src="linkedin.png"
-                        alt="social-icon"
-                    />
+                    <a
+                        href="https://www.linkedin.com/in/jacob-doherty1/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            id="linkedin-icon"
+                            className="social-icon"
+                            src={linkedin}
+                            alt="social-icon"
+                        />
+                    </a>
                 </div>
                 <div className="social-container">
-                    <img
-                        className="social-icon"
-                        src="linkedin.png"
-                        alt="social-icon"
-                    />
-                </div>
-                <div className="social-container">
-                    <img
-                        className="social-icon"
-                        src="linkedin.png"
-                        alt="social-icon"
-                    />
+                    <a
+                        href="https://github.com/Jake-Doherty"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            id="github-icon"
+                            className="social-icon"
+                            src={github}
+                            alt="social-icon"
+                        />
+                    </a>
                 </div>
             </section>
         </>
